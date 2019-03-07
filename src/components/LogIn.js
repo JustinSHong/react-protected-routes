f3384a14119c63564843a4041b5f1c64abba18cc
JustinSHong <jhong2009@gmail.com>
2019-03-06 21:29:58 -0500

    set up the LogIn to auth user

---
+++ src/components/LogIn.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import authService from "../utils/auth";
+
+class Login extends Component {
+    state = {
+        redirect: false
+    };
+
+    login = () => {
+        authService.authenticate(() => {
+            this.setState(() => ({
+                redirect: true
+            }));
+        });
+    };
+
+    render() {
+        const { redirect } = this.state;
+
+        if (redirect === true) {
+            return <Redirect to="/" />;
+        }
+
+        return (
+            <div>
+                <h1>LogIn</h1>
+                <button onClick={this.login}>Log In Here</button>
+            </div>
+        );
+    }
+}
+
+export default Login;
