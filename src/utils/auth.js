0ff4ef3e27f5a307469dc3fa3c652ad417efd907
JustinSHong <jhong2009@gmail.com>
2019-03-06 21:00:05 -0500

    created dummy auth service interface

---
+++ src/utils/auth.js
@@ -0,0 +1,13 @@
+const authService = {
+    isAuthenticated: false,
+    authenticate(callback) {
+        this.isAuthenticated = true;
+        setTimeout(callback, 100);
+    },
+    signOut(callback) {
+        this.isAuthenticated = false;
+        setTimeout(callback, 100);
+    }
+};
+
+export default authService;
