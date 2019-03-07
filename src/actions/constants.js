b7f04fc375655ac83ebd20c2024a0146a63052d6
JustinSHong <jhong2009@gmail.com>
2019-03-06 20:36:29 -0500

    set up redux

---
+++ src/actions/constants.js
@@ -0,0 +1,9 @@
+export const CREATE_USER = "CREATE_USER";
+export const CREATING_USER = "CREATING_USER";
+export const CREATE_USER_ERROR = "CREATE_USER_ERROR";
+export const LOG_IN_USER = "LOG_IN_USER";
+export const LOGGING_IN_USER = "LOGGING_IN_USER";
+export const LOG_IN_USER_ERROR = "LOG_IN_USER_ERROR";
+export const LOG_OUT_USER = "LOG_IN_USER";
+export const LOGGING_OUT_USER = "LOGGING_OUT_USER";
+export const LOG_OUT_USER_ERROR = "LOG_OUT_USER_ERROR";
---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import {
+    CREATE_USER,
+    CREATING_USER,
+    CREATE_USER_ERROR,
+    LOG_IN_USER,
+    LOGGING_IN_USER,
+    LOG_IN_USER_ERROR,
+    LOG_OUT_USER,
+    LOGGING_OUT_USER,
+    LOG_OUT_USER_ERROR
+} from "./constants";
+
+export const createUser = (user = dispatch => {
+    dispatch({ type: CREATING_USER });
+    try {
+        dispatch({ type: CREATE_USER, payload: user });
+    } catch {
+        dispatch({ type: CREATE_USER_ERROR });
+    }
+});
